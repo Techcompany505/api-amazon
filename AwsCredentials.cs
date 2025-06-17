95c91b036057380f5ac943be6092bd998eb42a11
Techcompany <crismunozgalo77@gmail.com>
2025-06-17 11:47:13 -0400

    Create AwsCredentials.cs

---
+++ AwsCredentials.cs
@@ -0,0 +1,7 @@
+// Models/AwsCredentials.cs
+public class AwsCredentials
+{
+    public string AccessKey { get; set; }
+    public string SecretKey { get; set; }
+    public string Region { get; set; }
+}
